213fcf8f42c8df0e64f0f0640131f13c4514139a
JonGunnar04 <jon.gunnar.karlsson@gmail.com>
2025-04-07 18:35:23 +0000

    Debug banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'gradient_container.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GradientContainer(
           Color.fromARGB(255, 200, 200, 175),
